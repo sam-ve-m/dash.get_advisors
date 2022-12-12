eeb203d592128176289271e4fe1cbd88c0a750ae
João Bonfim <joao.bonfim@lionx.com.br>
2022-12-12 13:51:59 -0300

    feat: endpoint to get advisors

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+fission spec init
+fission env create --spec --name env-get-advisor --image nexus.sigame.com.br/fission-cx-env-3.8:0.0.3 --builder nexus.sigame.com.br/fission-cx-builder-3.8:0.0.3
+fission fn create --spec --name fn-get-advisor --env user-review-env --src "./func/*" --entrypoint main.get_enums --executortype newdeploy
+fission route create --spec --name rt-get-advisor --method GET --url /enum/advisor --function fn-get-advisor
