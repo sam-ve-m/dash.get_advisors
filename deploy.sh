6fd911a20e26005be1e34f9424f906368d1d3f30
Samuel Veloso Marçal <samuel.marcal@ligainvest.com.br>
2023-01-09 13:13:14 -0300

    chore: added image to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 fission spec init
-fission env create --spec --name dash-enum-advisor-env --image newdeploy --poolsize 0 --version 3 --imagepullsecret "nexus-v3" --spec
+fission env create --spec --name dash-enum-advisor-env --image nexus.sigame.com.br/fission-ligadash-enum-advisor:0.1.1 --poolsize 0 --version 3 --imagepullsecret "nexus-v3" --spec
 fission fn create --spec --name dash-enum-advisor-fn --env dash-enum-advisor-env --code fission.py --targetcpu 80 --executortype newdeploy --maxscale 3 --requestsperpod 10000 --spec
 fission route create --spec --name dash-enum-advisor-rt --method GET --url /enum/advisor --function dash-enum-advisor-fn
